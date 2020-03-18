1e07f312d49788e5bb363cebf7658f1daaf27d05
Rich <richgrant192@gmail.com>
2020-03-18 14:53:42 +0000

    added app.rb

--- pokemon_controller.rb
+++ pokemon_controller.rb
@@ -1,6 +1,13 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('./db/sql_runner.rb')
-require_relative('./models/pokemon')
-require_relative('./models/nurse')
+require_relative('./models/pokemon.rb')
+require_relative('./models/nurse.rb')
 also_reload('./models/*')
+
+
+
+get '/pokemon' do
+  @pokemon = Pokemon.all()
+  erb(:'pokemon/index')
+end
