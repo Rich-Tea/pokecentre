fe070d34cfb1b6bec839b2da8098cc9540a99676
Rich <richgrant192@gmail.com>
2020-03-18 17:48:24 +0000

    adding restful routes - added get in app.rb - tested in sinatra & browser - no errors

--- app.rb
+++ app.rb
@@ -1,3 +1,12 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative
+require_relative('controllers/pokemon_controller.rb')
+require_relative('controllers/nurse_controller.rb')
+require_relative('models/pokemon.rb')
+require_relative('models/nurse.rb')
+also_reload('../models/*')
+
+
+get '/' do
+  erb(:index)
+end
--- controllers/pokemon_controller.rb
+++ controllers/pokemon_controller.rb
@@ -1,13 +1,12 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('./db/sql_runner.rb')
-require_relative('./models/pokemon.rb')
-require_relative('./models/nurse.rb')
+require_relative('../models/pokemon.rb')
+require_relative('../models/nurse.rb')
 also_reload('./models/*')
 
 
 
 get '/pokemon' do
   @pokemon = Pokemon.all()
-  erb(:'pokemon/index')
+  erb(:'index')
 end
