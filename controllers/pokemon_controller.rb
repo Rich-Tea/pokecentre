4c7c3f50243b326793ae642b323c7a8e2f34fff1
Rich <richgrant192@gmail.com>
2020-03-18 19:10:43 +0000

    finished restful routes in pokemon views

--- controllers/pokemon_controller.rb
+++ controllers/pokemon_controller.rb
@@ -14,3 +14,42 @@ end
 get '/pokemon/new' do # new
   erb(:'pokemon/new')
 end
+
+get '/pokemon/add' do # create
+  @pokemon = Pokemon.new(params)
+  @pokemon.save()
+  erb(:'pokemon/create')
+end
+
+get '/pokemon/:id' do
+  @pokemon = Pokemon.find(params['id'].to_i)
+  erb(:'pokemon/show')
+end
+
+get '/pokemon/:id/treatment-notes' do
+  @pokemon = Pokemon.find(params['id'])
+  @nurses = Nurse.all()
+  erb(:'pokemon/treatment')
+end
+
+post '/pokemon/:id/assign' do
+  pokemon = Pokemon.new(params)
+  pokemon.update()
+  redirect to '/pokemon'
+end
+
+get '/pokemon/:id/edit' do # edit
+  @pokemon = Pokemon.find(params[:id])
+  erb(':pokemon/edit')
+end
+
+post '/pokemon/:id/edit' do # update
+  Pokemon.new(params).update
+  redirect to '/pokemon'
+end
+
+post '/pokemon/:id/delete' do # delete
+  pokemon = Pokemon.find(params[:id])
+  pokemon.delete()
+  redirect to '/pokemon'
+end
